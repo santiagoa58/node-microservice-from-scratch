6c4df49d3718a8dc7647adc950c543661b8a29e2
Santi G <24705975+santiagoa58@users.noreply.github.com>
2023-11-16 19:33:18 +0000

    Refactor CommentList and PostList components.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,6 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        `http://localhost:4002/posts/${postId}/comments`
-      );
-
-      setComments(res.data);
-    };
-    fetchData();
-  }, [postId]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
