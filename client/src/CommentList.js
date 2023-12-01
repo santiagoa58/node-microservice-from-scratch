405125acadb7ca247baa02af064446bacc7818e3
Santi G <24705975+santiagoa58@users.noreply.github.com>
2023-12-01 03:16:49 +0000

    Refactor CommentList component to handle different comment statuses

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments
-    .filter((comment) => comment.status !== "rejected")
-    .map((comment) => {
-      return <li key={comment.id}>{comment.content}</li>;
-    });
+  const renderedComments = comments.map((comment) => {
+    let content;
+    switch (comment.status) {
+      case "approved":
+        content = comment.content;
+        break;
+      case "pending":
+        content = "This comment is awaiting moderation.";
+        break;
+      case "rejected":
+        content = "This comment has been rejected.";
+        break;
+      default:
+        break;
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
 
   return <ul>{renderedComments}</ul>;
 };
